7f3e3a73e7201a3abccda375f6d45ee573f08542
Admin <overgeardev@gmail.com>
2025-06-11 20:57:31 +0300

    - autosave/load feature

--- JS/11_06 task/script.js
+++ JS/11_06 task/script.js
@@ -10,23 +10,11 @@ addBlock.onclick = () => {
     block.style.height = '100px';
     blocksDiv[0].appendChild(block);
     blocks.push(block);
-};
-
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-saveBtn.onclick = () => {
     const colors = blocks.map(block => block.style.backgroundColor);
     localStorage.setItem('blocks', JSON.stringify(colors));
 };
 
-loadBtn.onclick = () => {
+document.body.onload = () => {
     const colors = JSON.parse(localStorage.getItem('blocks') || '[]');
     blocksDiv[0].innerHTML = '';
     blocks = [];
@@ -39,4 +27,14 @@ loadBtn.onclick = () => {
         blocksDiv[0].appendChild(block);
         blocks.push(block);
     });
-};
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
