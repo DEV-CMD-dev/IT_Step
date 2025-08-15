93d4ee0cf1e472c966054ed558e85a8d9473ee08
Admin <overgeardev@gmail.com>
2025-08-15 15:03:00 +0300

    - added fav button

--- React/shop-final/src/components/ProductItem.jsx
+++ React/shop-final/src/components/ProductItem.jsx
@@ -1,26 +1,34 @@
 import { useState } from 'react';
 import './ProductItem.css'
 
-export default function ProductItem({ image, title, rating, stock ,price }){
+export default function ProductItem({ id, image, isFavorite, title, rating, stock, price, onToggleFavorite }) {
     const inStock = stock > 0
     const [imgError, setImgError] = useState(false);
-    
+
+
 
     const imageSrc = !image || imgError ? '/images/imagePlaceholder.png' : image;
     const altText = !image || imgError ? 'No photo' : 'Product photo';
-    
-    return(
+
+    return (
         <div className={"ProductItem" + (inStock ? "" : " inactiveItem")}>
-            <img
-                src={imageSrc}
-                alt={altText}
-                onError={() => setImgError(true)}
+            <span>
+                <img
+                    src={imageSrc}
+                    alt={altText}
+                    onError={() => setImgError(true)}
                 />
-            <h2 className="ellipsis">{title}</h2>
+                <button onClick={() => onToggleFavorite(id)} className='favoriteButton'>
+                    <img src={isFavorite ? '/images/Favorite.png' : '/images/nonFavorite.png'}></img>
+                </button>
+            </span>
+            <h2 className="ellipsis" style={{ color: isFavorite ? 'yellow' : '' }}>{title}</h2>
             <div>{rating}/5</div>
-            <h3 className={inStock ? "inStock" : "outOfStock" }>{inStock ? "In stock" : "Out of stock"}</h3>
+            <h3 className={inStock ? "inStock" : "outOfStock"}>{inStock ? "In stock" : "Out of stock"}</h3>
             <h2>{price}$</h2>
-            <button>Add to Cart</button>
+
+            <button className='addToCartButton'>Add to Cart</button>
+
         </div>
     )
 }
--- React/shop-final/src/components/ProductList.jsx
+++ React/shop-final/src/components/ProductList.jsx
@@ -1,7 +1,22 @@
+import { useState } from 'react';
 import ProductItem from './ProductItem'
 import './ProductList.css'
+import { v4 as uuidv4 } from "uuid";
 
 export default function ProductList({ products }) {
+
+  const [favorites, setFavorites] = useState([]);
+
+  const toggleFavorites = (id) => {
+    if (favorites.includes(id)) {
+      console.log("removed")
+      setFavorites(favorites.filter(favId => favId !== id));
+    } else {
+      console.log("added")
+      setFavorites([...favorites, id]);
+    }
+  };
+
 
   if (!products || products.length === 0) {
     return <p>No products available.</p>;
@@ -11,12 +26,15 @@ export default function ProductList({ products }) {
     <div className="productList">
       {products.map((product, index) => (
         <ProductItem
-          key={product._id || index}
+          key={index}
+          id={uuidv4()}
           image={product.image}
+          isFavorite={favorites.includes(product.id)}
           title={product.title}
           rating={product.rating}
           stock={product.stock}
           price={product.price}
+          onToggleFavorite={() => toggleFavorites(product.id)}
         />
       ))}
     </div>
